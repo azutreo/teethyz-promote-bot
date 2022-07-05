1ae908115d081573abe906be065052cfcc366ed0
Nicholas Foreman <nicholas.w.foreman@outlook.com>
2022-07-04 23:27:13 -0500

    Print request

--- application.js
+++ application.js
@@ -17,28 +17,30 @@ Application.get("/", (request, response) => {
 	response.send("Website application for Teethyz Promote Bot. Nothing to see here if you do not belong here.");
 });
 
-Application.get("/log", (request, response) => {
+Application.get("/log/:api_key/:hrUserId/:hrUsername/:lrUsername/:lrRole", (request, response) => {
 	console.log(request);
-	/*const apiKey = request.params.api_key;
-	const user1 = request.params.user_id1;
-	const user2 = request.params.user_id2;
+
+	const apiKey = request.params.api_key;
+	const hrUserId = request.params.hrUserId;
+	const hrUsername = request.params.hrUsername;
+	const lrUsername = request.params.lrUsername;
 
 	if (apiKey != process.env.API_KEY) {
 		return response.json("Error");
 	}
 
 	const embed = new MessageEmbed()
-		.setTitle('Some Title')
-		.setColor('#0099ff');
+		.setTitle("Promotion!")
+		.setColor("#0099ff")
+		.setDescription(`**${hrUsername}** successfully promoted **${lrUsername}** to **${lrRole}**`);
 
 	webhookClient.send({
-		content: 'Webhook test',
-		username: 'some-username',
-		avatarURL: 'https://i.imgur.com/AfFp7pu.png',
+		username: hrUsername,
+		avatarURL: `https://www.roblox.com/headshot-thumbnail/image?userId=${hrUserId}&width=420&height=420&format=png`,
 		embeds: [embed],
 	});
 
-	response.json("Success");*/
+	response.json("Success");
 });
 
 Application.get("/promote/:api_key/:user_id", (request, response) => {
