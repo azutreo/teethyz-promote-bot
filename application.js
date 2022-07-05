adcb615f5d47a14232d630305c381ead8c0cfb81
Nicholas Foreman <nicholas.w.foreman@outlook.com>
2022-07-05 00:49:04 -0500

    Seperate promote/demote

--- application.js
+++ application.js
@@ -5,12 +5,17 @@ require("dotenv").config();
 
 const GROUP_ID = process.env.GROUP_ID
 const RANK_PROMOTER = process.env.RANK_PROMOTER
+const RANK_DEMOTER = process.env.RANK_DEMOTER
 const RANK_MAX = process.env.RANK_MAX
 const RANK_MIN = process.env.RANK_MIN
 const API_KEY = process.env.API_KEY
 const WEBHOOK_ID = process.env.WEBHOOK_ID
 const WEBHOOK_TOKEN = process.env.WEBHOOK_TOKEN
 const COOKIE = process.env.COOKIE
+
+const COLOR_PROMOTION = "#4caf50"
+const COLOR_DEMOTION = "#ef5350"
+const COLOR_ERROR = "#455a64"
 
 const Application = Express();
 Application.use(Express.static("public"));
@@ -26,20 +31,20 @@ Application.get("/", (request, response) => {
 	response.send("Website application for Teethyz Promote Bot. Nothing to see here if you do not belong here.");
 });
 
-async function LogSuccess(hrUserId, lrUserId, lrPreviousRankName) {
+async function LogSuccess(delta, hrUserId, lrUserId, lrPreviousRankName) {
 	let hrUsername = await Noblox.getUsernameFromId(hrUserId)
 	let lrUsername = await Noblox.getUsernameFromId(lrUserId)
 	let lrNewRankName = await Noblox.getRankNameInGroup(GROUP_ID, lrUserId);
 
 	const embed = new MessageEmbed()
-		.setColor("#4caf50")
+		.setColor(delta > 0 ? COLOR_PROMOTION : COLOR_DEMOTION)
 		.setAuthor({
 			name: `${hrUsername} [${hrUserId}]`,
 			iconURL: `https://www.roblox.com/headshot-thumbnail/image?userId=${hrUserId}&width=420&height=420&format=png`,
 			url: `https://www.roblox.com/users/${hrUserId}/profile`
 		})
 		.setThumbnail(`https://www.roblox.com/headshot-thumbnail/image?userId=${lrUserId}&width=420&height=420&format=png`)
-		.addField("Promoted Staff", `${lrUsername} [${lrUserId}]`, true)
+		.addField("Staff Member", `${lrUsername} [${lrUserId}]`, true)
 		.addField("Role Change", `${lrPreviousRankName} → ${lrNewRankName}`)
 		.setTimestamp()
 		.setFooter({ text: 'Created by Azutreo', iconURL: 'https://www.roblox.com/headshot-thumbnail/image?userId=9221415&width=420&height=420&format=png' });;
@@ -51,23 +56,72 @@ async function LogSuccess(hrUserId, lrUserId, lrPreviousRankName) {
 	});
 }
 
-async function Promote(hrUserId, lrUserId) {
+async function LogError(errorCode, delta, hrUserId, lrUserId) {
+	let hrUsername = await Noblox.getUsernameFromId(hrUserId)
+	let lrUsername = await Noblox.getUsernameFromId(lrUserId)
+
+	let error;
+	switch (errorCode) {
+		case -2:
+			error = "Promoter is too low rank (must be Assistant Manager+)"
+			break;
+		case -3:
+			error = "Demoter is too low rank (must be Dental Board+)"
+			break;
+		case -4:
+			error = "Staff member is too low rank (must at least be Awaiting Training)"
+			break;
+		case -5:
+			error = "Staff member is too high rank (must at under be Oral Surgeon)"
+			break;
+		default:
+			error = "Unknown (contact Azutreo for error log)"
+	}
+
+	const embed = new MessageEmbed()
+		.setColor(COLOR_ERROR)
+		.setDescription(`Failed to ${delta > 0 ? "promote" : "demote"}. Error: ${errorCode}`)
+		.setAuthor({
+			name: `${hrUsername} [${hrUserId}]`,
+			iconURL: `https://www.roblox.com/headshot-thumbnail/image?userId=${hrUserId}&width=420&height=420&format=png`,
+			url: `https://www.roblox.com/users/${hrUserId}/profile`
+		})
+		.setThumbnail(`https://www.roblox.com/headshot-thumbnail/image?userId=${lrUserId}&width=420&height=420&format=png`)
+		.addField("Staff Member", `${lrUsername} [${lrUserId}]`, true)
+		.addField("Role", lrPreviousRankName)
+		.setTimestamp()
+		.setFooter({ text: 'Created by Azutreo', iconURL: 'https://www.roblox.com/headshot-thumbnail/image?userId=9221415&width=420&height=420&format=png' });
+
+	webhookClient.send({
+		username: "Teethyz Promotion Bot",
+		avatarURL: `https://tr.rbxcdn.com/138f16052ce9c587817e282b4b7c6fa8/150/150/Image/Png`,
+		embeds: [embed],
+	});
+}
+
+async function ChangeRank(hrUserId, lrUserId, delta) {
 	let hrRank = await Noblox.getRankInGroup(GROUP_ID, hrUserId);
 	let lrRank = await Noblox.getRankInGroup(GROUP_ID, lrUserId);
 	let lrPreviousRankName = await Noblox.getRankNameInGroup(GROUP_ID, lrUserId);
 
-	/*if (hrRank < RANK_PROMOTER) {
+	if (delta > 0 && hrRank < RANK_PROMOTER) {
+		LogError(-2, delta, hrRank, lrRank, lrPreviousRankName);
 		return -2;
-	}*/
-
-	if (lrRank >= RANK_MAX) {
+	} else if (hrRank < RANK_DEMOTER) {
+		LogError(-3, delta, hrRank, lrRank, lrPreviousRankName);
 		return -3;
-	} else if (lrRank < RANK_MIN) {
-		return -4;
 	}
 
-	await Noblox.changeRank(GROUP_ID, lrUserId, 1);
-	await LogSuccess(hrUserId, lrUserId, lrPreviousRankName);
+	if (delta > 0 && lrRank >= RANK_MAX || lrRank > RANK_MAX) {
+		LogError(-4, delta, hrRank, lrRank, lrPreviousRankName);
+		return -4;
+	} else if (lrRank < RANK_MIN) {
+		LogError(-5, delta, hrRank, lrRank, lrPreviousRankName);
+		return -5;
+	}
+
+	await Noblox.changeRank(GROUP_ID, lrUserId, delta);
+	await LogSuccess(delta, hrUserId, lrUserId, lrPreviousRankName);
 
 	return 1;
 }
@@ -81,7 +135,21 @@ Application.get("/promote/:api_key/:hrUserId/:lrUserId", (request, response) => 
 		return response.json("Error -1");
 	}
 
-	Promote(hrUserId, lrUserId);
+	ChangeRank(hrUserId, lrUserId, 1);
+
+	response.json("Success");
+});
+
+Application.get("/demote/:api_key/:hrUserId/:lrUserId", (request, response) => {
+	const apiKey = request.params.api_key;
+	const hrUserId = parseInt(request.params.hrUserId);
+	const lrUserId = parseInt(request.params.lrUserId);
+
+	if (apiKey != API_KEY) {
+		return response.json("Error -1");
+	}
+
+	ChangeRank(hrUserId, lrUserId, -1);
 
 	response.json("Success");
 });
